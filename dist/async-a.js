1650b154a046532d00b72db404a3ee0332499db8
willTerner <ternerwill7@gmail.com>
2022-04-07 23:35:29 +0800

    Initial commit

---
+++ dist/async-a.js
@@ -0,0 +1,63 @@
+(self["webpackChunknode_starter"] = self["webpackChunknode_starter"] || []).push([[794],{
+
+/***/ 956:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ a)
+});
+
+// EXTERNAL MODULE: ./src/d.js
+var d = __webpack_require__(673);
+;// CONCATENATED MODULE: ./src/e.js
+/* harmony default export */ const e = ('e');
+
+// EXTERNAL MODULE: ./src/node_modules/x.js
+var x = __webpack_require__(934);
+var x_default = /*#__PURE__*/__webpack_require__.n(x);
+// EXTERNAL MODULE: ./src/node_modules/y.js
+var y = __webpack_require__(616);
+var y_default = /*#__PURE__*/__webpack_require__.n(y);
+;// CONCATENATED MODULE: ./src/a.js
+
+
+
+
+/* harmony default export */ const a = ('a' + d/* default */.Z + e + (x_default()) + (y_default()));
+__webpack_require__.e(/* import() | async-g */ 137).then(__webpack_require__.bind(__webpack_require__, 487));
+
+
+/***/ }),
+
+/***/ 673:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ('d');
+
+
+/***/ }),
+
+/***/ 934:
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ 616:
+/***/ (() => {
+
+
+
+/***/ })
+
+}]);
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+/**
+ * @type {import("webpack/types").Configuration}
+ */
+const config = {
+  mode: 'production',
+  entry: {
+    main: './src',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    clean: true,
+  },
+  optimization: {
+    // Instruct webpack not to obfuscate the resulting code
+    minimize: false,
+    splitChunks: {
+      minSize: 0,
+    },
+  },
+  context: __dirname,
+};
+
+module.exports = config;
